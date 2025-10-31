cd47a211f97de42b6d06daf60781712aca82e527
Alejandra González <aleja.gonzalezc@duocuc.cl>
2025-10-30 22:10:23 -0300

    feat: navegacin entre favoritos y otras clasess

--- app/src/main/java/com/example/rosapastelapp/ui/Carrito.kt
+++ app/src/main/java/com/example/rosapastelapp/ui/Carrito.kt
@@ -158,7 +158,7 @@ private fun BottomNavBarPrincipal(
         // favoritos
         NavigationBarItem(
             selected = itemSeleccionado == "Favorites",
-            onClick = { /* clase por crear */ },
+            onClick = { viewModel.navigateTo(Screen.Favorites) },
             icon = {
                 Icon(
                     imageVector = Icons.Filled.FavoriteBorder,
